009874fc5dfac7774afd2d646b0f9c5c9cdf9bb4
imrxn1 <305736762+imrxn1@users.noreply.github.com>
2026-07-19 15:56:39 +0000

    day 2 of learning js

--- Day1/app.js
+++ Day1/app.js
@@ -5,4 +5,41 @@ console.log(window.randomGuyd, window.animal);
 
 //Data Types
 
-//
+//objects
+
+const person = {
+    name: "Imran",
+    age: 18,
+    country: "India"
+};
+
+console.log (person.name);
+console.log (person["name"]);
+
+person.age = 22;
+console.log(person);
+
+person.city = "Nalhati";
+console.log(person);
+
+delete person.city;
+console.log(person);
+
+//symbol
+
+const id = Symbol();
+
+const user = {
+    name: "Imran",
+    
+    [id] : 123
+};
+
+console.log(user[id]);
+
+//Symbol is used to create unique object property keys so they don't accidentally clash with normal property names.
+
+//Big int
+
+const b = BigInt("1883283683662362636239289383");
+console.log(b);
---
+++ day2/day2.js
@@ -0,0 +1,18 @@
+let random = 2;
+ random = true;
+
+ let msg = `hey ${24} imran`;
+ console.log(msg);
+
+  Number("22");
+
+  //clousers
+
+  let myName = "JS";
+  let dept = "web";
+
+  function show() {
+
+  }
+
+  console.log(show);
