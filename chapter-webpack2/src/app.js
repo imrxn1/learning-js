6821d92f60ba46f4773d207a9d1654f8cf0d3ca1
imrxn1 <305736762+imrxn1@users.noreply.github.com>
2026-09-10 10:01:25 +0000

    learned async js

--- chapter-webpack2/src/app.js
+++ chapter-webpack2/src/app.js
@@ -1,58 +1,209 @@
-// iterator
-// const arr = [1,2,3];
+// createOrder(cart, (orderId) => {
+//   proceedToPayment(orderId);
+// });
 
-// for(let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+// const promise = createOrder(cart);
 
-// function createIterator(collection) {
-//     let i = 0;
-//     return {
-//         next() {
-//             return {
-//                 done: i >= collection.length,
-//                 value: collection[i++]
-//             }
-//         }
+// //{data: undefined}
+
+// promise
+// .then((orderId) =>
+//     proceedToPayment(orderId));
+
+// const GITHUB_API = "https://api.github.com/users/imrxn1";
+// const user = fetch(GITHUB_API);
+// console.log(user);
+
+// user.then(data => console.log(data));
+
+//real world example
+// const cart = ["shoes", "pants", "shirt"];
+
+// const promise = createOrder(cart); //orderID
+
+// promise.then(() => {
+//   proceedToPayment(orderId);
+// });
+
+// createOrder(cart)
+//   .then((orderID) => orderID)
+//   .then((orderID) => proceedToPayment(orderID))
+//   .then((message) => console.log(message))
+//   .catch((err) => console.log(err.message));
+
+// function createOrder(cart) {
+//   const promise = new Promise((resolve, reject) => {
+//     //create an order
+//     //validateCart
+//     //orderId
+//     if (!validate(cart)) {
+//       const err = new Error("cart is not valid");
+//       reject(err);
 //     }
-// }
-
-// let itrate = createIterator(arr);
-
-// let itrate = arr[Symbol.iterator]();
-// console.log(itrate.next());
-// console.log(itrate.next());
-// console.log(itrate.next());
-// console.log(itrate.next());
-
-
-// let str = 'text';
-// let itrate1  = str[Symbol.iterator]();
-// console.log(itrate1.next());
-// console.log(itrate1.next());
-// console.log(itrate1.next());
-// console.log(itrate1.next());
-// console.log(itrate1.next());
-
-// for of loop
-
-// const arr = [1,2,3];
-
-// for(let v of arr) {
-//     console.log(v); 
-// }
-
-//generator
-// const arr = [1,2,3];
-// function* generate(collection) {
-//     for(let i = 0; i < collection.length; i++) {
-//         yield collection[i]
+//     // logic for create order
+//     const orderID = "12345";
+//     if (orderID) {
+//       setTimeout(() => {
+//         resolve(orderID);
+//       }, 5000);
 //     }
+//   });
+
+//   return promise;
 // }
 
-// let it = generate(arr)
-// console.log(it.next());
-// console.log(it.next());
-// console.log(it.next());
-// console.log(it.next());
+// function proceedToPayment(orderID) {
+//   return new Promise((resolve, reject) => {
+//     resolve("payment susessful");
+//   });
+// }
 
+// function validate(cart) {
+//   return true;
+//   // return false;
+// }
+
+// hw
+
+// const cart = ["jeans", "shirt", "sneaker", "jacket", "tshirt"];
+
+// createOrder(cart)
+//   .then((orderID,cart) => proceedToPayment(orderID, cart))
+//   .then((order) => {
+//     console.log(order.message);
+//     showOrderSummery(order.cart);
+//   });
+
+// // createOrder
+// function createOrder(cart) {
+//   return new Promise((resolve, reject) => {
+//     if (!validCart(cart)) {
+//       const err = new Error("cart is not valid");
+//       reject(err);
+//     }
+//     const orderId = "12345";
+//     resolve(orderId,cart);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
+// }
+
+// // proceedToPayment
+// function proceedToPayment(orderID, cart) {
+//   return new Promise((resolve, reject) => {
+//     resolve({
+//       orderID: orderID,
+//       message: "payment was sucessful",
+//       cart: cart,
+//     });
+//   });
+// }
+
+// // showOrderSummery
+// function showOrderSummery(cart) {
+//   for (let index = 0; index < cart.length; index++) {
+//     console.log(cart[index]);
+//   }
+// }
+
+// //validCart
+// function validCart() {
+//   return true;
+// }
+
+// promise api
+
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("p1 sucess");
+//     // reject("p1 failure");
+//   }, 3000);
+// });
+
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("p2 sucess");
+//     // reject("p2 failure");
+//   }, 1000);
+// });
+
+// const p3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("p3 sucess");
+//     // reject("p3 failure");
+//   }, 2000);
+// });
+
+// Promise.all([p1, p2, p3])
+// .then((res) => console.log(res))
+// .catch((err)=> console.error(err)
+// );
+
+// Promise.allSettled([p1, p2, p3])
+// .then((res) => console.log(res));
+
+// Promise.race([p1, p2, p3])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
+
+// Promise.any([p1, p2, p3])
+//   .then((res) => console.log(res))
+//   .catch((err) => {
+//     console.log(err);
+//     console.log(err.errors);
+//   });
+
+// Async-Await
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("promise resolved value");
+//   }, 5000);
+// });
+
+// const promise2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("promise resolved value");
+//   }, 3000);
+// });
+
+//handeling promise
+
+// function getData() {
+//   //js engine will not wait for promise to resolved
+//   promise.then((value) => console.log(value));
+//   console.log("hi imran");
+// }
+
+// getData();
+
+// async function handelPromise() {
+//   console.log("hello");
+
+//   const value = await promise;
+//   console.log("hi imran 1");
+//   console.log(value);
+
+//   const value2 = await promise2;
+//   console.log("hi imran 2");
+//   console.log(value);
+// }
+
+// handelPromise();
+
+// real world example
+
+// const API_URL = "https://api.github.com/users/imrxn1";
+
+// async function handelPromise() {
+//   try {
+//     const data = await fetch(API_URL);
+//     const jsonValue = await data.json();
+//     console.log(jsonValue);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// handelPromise();
